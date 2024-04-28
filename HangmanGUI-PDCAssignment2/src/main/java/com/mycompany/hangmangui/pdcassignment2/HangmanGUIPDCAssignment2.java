3d0a30a2d4e1861e3407de31bd3c26746660699a
goofyjnr <goofyjnr@gmail.com>
2024-04-29 11:52:28 +1200

    first commit
    seting up the project folder

---
+++ HangmanGUI-PDCAssignment2/src/main/java/com/mycompany/hangmangui/pdcassignment2/HangmanGUIPDCAssignment2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hangmangui.pdcassignment2;
+
+/**
+ *
+ * @author alist
+ */
+public class HangmanGUIPDCAssignment2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
