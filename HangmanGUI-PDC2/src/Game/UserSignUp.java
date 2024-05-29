49484be8e930676dcd968beef1f0b0baf8e6975d
jhavvv <javeriaalinz@gmail.com>
2024-05-29 15:38:59 +1200

    Refractor UserSignup class to use the new UserDAO database

--- HangmanGUI-PDC2/src/Game/UserSignUp.java
+++ HangmanGUI-PDC2/src/Game/UserSignUp.java
@@ -1,41 +1,36 @@
 package Game;
-
-import java.util.Scanner;
-
 /**
  *
  * @author javer
  */
-public class UserSignUp {
-    private static final Scanner scanner = new Scanner(System.in);
+import java.util.Scanner;
+
+public class UserSignUp{
     
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void signUp(){
-        System.out.println("Sign up for a Hangman Account!!");
+        System.out.println("Sign up for a Hangman Game Account!!");
         System.out.println("Username: ");
         String username = scanner.nextLine();
         System.out.println("Password: ");
         String password = scanner.nextLine();
-        
-        if (isValidUsername(username) && isValidUsername(password)){
+
+        if (isValidUsername(username) && isValidPassword(password)) {
             String hashedPassword = PasswordManager.hashPassword(password);
-            UsernameManager.saveUsername(username, hashedPassword);
-            
-            System.out.println("Sign up successful ");
-        }   else {
+            UserDAO.addUser(username, hashedPassword);
+
+            System.out.println("Sign up has been successful");
+        } else {
             System.out.println("Please try again. Invalid username and/or password.");
         }
     }
-    
-    private static boolean isValidUsername(String username){
+
+    private static boolean isValidUsername(String username) {
         return !username.isEmpty();
     }
-    
-    private static boolean isValidPassword(String password){
+
+    private static boolean isValidPassword(String password) {
         return password.length() >= 6;
     }
-      
-    private static void storeCredentials(String username, String hashedPassword){
-        System.out.println("Storing username and password...");
-    }
-    
 }
