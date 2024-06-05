16e0b5988b5013e63dab325301d1494ef570a990
jhavvv <javeriaalinz@gmail.com>
2024-06-06 01:19:31 +1200

    remove scanner for scanning username and password, it only takes in password and username

--- HangmanGUI-PDC2/src/Game/UserSignUp.java
+++ HangmanGUI-PDC2/src/Game/UserSignUp.java
@@ -5,27 +5,18 @@ package Game;
  */ 
 import java.util.Scanner;
 
-public class UserSignUp {
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void signUp() {
-        System.out.println("Sign up for a Hangman Account!!");
-        System.out.println("Username: ");
-        String username = scanner.nextLine();
-        System.out.println("Password: ");
-        String password = scanner.nextLine();
-
-        if (isValidUsername(username)) {
-            if (isValidPassword(password)) {
-                String hashedPassword = PasswordManager.hashPassword(password);
-                System.out.println("Username and password are valid. Attempting to add user to database.");
-                UserDAO.addUser(username, hashedPassword);
-                System.out.println("Sign up has been successful");
-            } else {
-                System.out.println("Password must be at least 6 characters long. Please try again.");
-            }
+public class UserSignUp{
+    
+    public static boolean signUp(String username, String password) {
+        if (isValidUsername(username) && isValidPassword(password)) {
+            String hashedPassword = PasswordManager.hashPassword(password);
+            System.out.println("Username and password are valid. Attempting to add user to database.");
+            UserDAO.addUser(username, hashedPassword);
+            System.out.println("Sign up has been successful");
+            return true;
         } else {
-            System.out.println("Invalid username. Please try again.");
+            System.out.println("Invalid username or password. Please try again.");
+            return false;
         }
     }
 
