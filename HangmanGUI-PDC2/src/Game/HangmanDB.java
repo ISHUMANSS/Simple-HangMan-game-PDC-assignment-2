6c415a9c93fafc58367aabc1a35594a06501d1d0
jhavvv <javeriaalinz@gmail.com>
2024-06-05 23:01:43 +1200

    Updated so that it takes in words and users and saves to database

--- HangmanGUI-PDC2/src/Game/HangmanDB.java
+++ HangmanGUI-PDC2/src/Game/HangmanDB.java
@@ -8,60 +8,33 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class HangmanDB {
-
+    private static HangmanDB instance;
+    private Connection connection;
     private static final String USER_NAME = "hangman";
     private static final String PASSWORD = "hangedman";
     private static final String URL = "jdbc:derby://localhost:1527/HangmanDB";
-
-    private static HangmanDB instance;
-    private Connection conn;
-
-    private HangmanDB() {
-        establishConnection();
-    }
-
-    public static HangmanDB getInstance() {
-        if (instance == null) {
-            instance = new HangmanDB();
+    
+    private HangmanDB() throws SQLException {
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver"); // Ensures correct driver
+            this.connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
         }
-        return instance;
     }
 
     public Connection getConnection() {
-        if (conn == null) {
-            establishConnection();
-        }
-        return this.conn;
+        return connection;
     }
 
-    private void establishConnection() {
-        try {
-            Class.forName("org.apache.derby.jdbc.ClientDriver");
-            conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
-            if (conn != null) {
-                System.out.println("Connected to database.");
-            }
-        } catch (ClassNotFoundException e) {
-            System.err.println("Derby JDBC Driver not found.");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            System.err.println("SQL Exception: " + e.getMessage());
-            e.printStackTrace();
-        } catch (Exception e) {
-            System.err.println("Exception: " + e.getMessage());
-            e.printStackTrace();
+    public static HangmanDB getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new HangmanDB();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new HangmanDB();
         }
-    }
 
-    public void closeConnections() {
-        if (conn != null) {
-            try {
-                conn.close();
-                System.out.println("Connection closed.");
-            } catch (SQLException ex) {
-                System.err.println("Error closing connection: " + ex.getMessage());
-                ex.printStackTrace();
-            }
-        }
+        return instance;
     }
 }
