51a93b78262a4f31b171d0dcbc1c60e879a66699
jhavvv <javeriaalinz@gmail.com>
2024-06-05 16:17:41 +1200

    Update WordDAO so that it gets words from the database, and add new words to database also.

--- HangmanGUI-PDC2/src/Game/WordDAO.java
+++ HangmanGUI-PDC2/src/Game/WordDAO.java
@@ -1,5 +1,8 @@
 package Game;
-
+/**
+ *
+ * @author javer
+ */
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,24 +10,29 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;  
 
-
-/**
- *
- * @author javer
- */
 public class WordDAO {
 
+    /**
+     * This adds a word to the database.
+     * @param word the word to be added
+     */
     public static void addWord(String word) {
         String sql = "INSERT INTO WORDS (WORD) VALUES (?)";
         try (Connection conn = HangmanDB.getInstance().getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, word);
             pstmt.executeUpdate();
+            System.out.println("Word added: " + word);
         } catch (SQLException e) {
+            System.err.println("Error adding word: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
+    /**
+     * retrieves words from the database. 
+     * @return a list of words
+     */
     public static List<String> getAllWords() {
         String sql = "SELECT WORD FROM WORDS";
         List<String> words = new ArrayList<>();
