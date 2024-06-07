a1359517568314a387006940fc2fd4258e9252a7
Is_human <goofyjnr@gmail.com>
2024-06-07 16:51:38 +1200

    Improved the test cases
    Now the test cases test more important things

--- HangmanGUI-PDC2/test/Game/WordCheckerTest.java
+++ HangmanGUI-PDC2/test/Game/WordCheckerTest.java
@@ -68,9 +68,6 @@ public class WordCheckerTest {
             }
             
         }
-        
-        
-        
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
     }
