e71ec25ba3fd7749424569bcaaa607f151aa221e
jhavvv <javeriaalinz@gmail.com>
2024-06-07 17:02:43 +1200

    updates

--- HangmanGUI-PDC2/src/Game/UserDAO.java
+++ HangmanGUI-PDC2/src/Game/UserDAO.java
@@ -8,23 +8,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserDAO{
-    public static void addUser(String username, String password){
+public class UserDAO {
+    public static void addUser(String username, String password) {
         String sql = "INSERT INTO USERS (USERNAME, PASSWORD) VALUES (?, ?)";
         try (Connection conn = HangmanDB.getInstance().getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, username);
             pstmt.setString(2, password);
             pstmt.executeUpdate();
-        }   catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static boolean verifyUser(String username, String password){
+    public static boolean verifyUser(String username, String password) {
         String sql = "SELECT * FROM USERS WHERE USERNAME = ? AND PASSWORD = ?";
         try (Connection conn = HangmanDB.getInstance().getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)){
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, username);
             pstmt.setString(2, password);
             try (ResultSet rs = pstmt.executeQuery()) {
